5676b50fdb8056dcc164181c7b33af7325dcee45
Tkoher <tanya_suz@hotmail.com>
2019-01-26 21:31:11 -0800

    commiting code for Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) { # set function resets matrix & resets inv to NULL in parent environment
+    x <<- y # resets the value of x to y in the parent environment
+    inv <<- NULL # resets value of inv to NULL in parent environment
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting the cached matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...) # find 1/x
+  x$setInverse(inv) 
+  inv
 }
